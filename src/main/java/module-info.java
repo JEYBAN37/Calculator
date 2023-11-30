e26aa59a2e17480abf7c0d8a0d7137ecc7d37346
Jeison Getial <jeyban37@gmail.com>
2023-11-29 22:39:05 -0500

    Resolve changues gradlew

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,6 @@
 module com.example.calculator_calidad_test {
     requires javafx.controls;
     requires javafx.fxml;
-
-
-
     opens com.example.calculator_calidad_test to javafx.fxml;
     exports com.example.calculator_calidad_test;
 }
